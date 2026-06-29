e323f86b1a547c853aaad39245b5f85977a35388
Alejandro Villaescusa <alexvillhen@gmail.com>
2026-06-28 20:30:21 -0700

    added create post API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js'
 
 const app = express();
 
@@ -7,6 +8,7 @@ app.use(express.json());
 
 //route declaration
 app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/posts", postRouter);
+
 
 export default app;
